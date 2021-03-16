7d18d73b8ddc788c68e9a517ab33b1532a562907
Public View Token <76509668+publicViewToken@users.noreply.github.com>
2021-03-15 18:50:17 -0700

    Add files via upload

---
+++ components/images.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import "./images.css";
+import background from "../img/tile002.png";
+import background2 from "../img/tile018.png";
+
+import img0 from "../img/tile000.png";
+import img1 from "../img/tile001.png";
+import img2 from "../img/tile002.png";
+import img3 from "../img/tile003.png";
+import img4 from "../img/tile004.png";
+import img5 from "../img/tile005.png";
+import img6 from "../img/tile006.png";
+
+
+import anim from "../img/anim.gif";
+import anim2 from "../img/anime3.gif";
+import creator from "../img/creatorsim.gif";
+import creator2 from "../img/creator.gif";
+import programmer from "../img/programmer.gif";
+
+export default [
+
+   
+   creator2,programmer,anim2, anim2,anim2,anim2, anim2,anim2,anim2,anim2,
+   anim2,anim2,anim2, anim2,anim2,anim2, anim2,anim2,anim2,anim2,
+   anim2,anim2,anim2, anim2,anim2,anim2, anim2,anim2,anim2,anim2,
+   anim2,anim2,anim2, anim2,anim2,anim2, anim2,anim2,anim2,anim2,
+   anim2,anim2,anim2, anim2,anim2,anim2, anim2,anim2,anim2,anim2,
+    img2,img3,img4,img5,img6,
+ 
+
+   
+   
+];
---
+++ contracts/Color.sol
@@ -0,0 +1,237 @@
+pragma solidity 0.5.0;
+
+import "./erc721full.sol";
+
+contract Color is ERC721 {
+
+
+  mapping(string => bool) _botNameExists;
+  
+   mapping (uint => address) public monstersIndexToAddress;
+    
+   address payable public owner;// address is payable
+   uint256 public totalSupply;
+  string public name;
+    string public symbol;
+    uint8 public decimals;
+  uint256 public claimPrice = 0.03 ether; //8-10$ at time of coding swutch to bnb prices whe on bnb
+          uint256 public changeItemPrice = 0.3 ether; //80 cnets
+         
+          uint256 public changePrice = 0.0003 ether;// 10 cent
+          uint256 public likesPrice = 0.0001 ether; //3 cents;
+
+     uint public id ;
+      simWearables[] public sims; //sim wearables
+ Digitmon[] public digitmon; // where all sims go 
+ uint level;
+ uint health;
+ string weapon;
+ 
+ struct simWearables{  // items people can buy for their sims/ can be place within block
+       uint a;
+       uint b;
+       uint c;
+       uint d;
+       
+       
+   }
+ 
+
+ struct Digitmon{   //all the sims
+     
+         string name; //mintabel
+        string gender;
+        string hairColor;
+        string eyeColor;
+       string  headColor;
+       string secret;
+       uint likes;
+      address payable simowner;
+
+    
+    
+       
+      
+ }
+ 
+   constructor() public
+   {
+       owner = msg.sender;
+      name = "Crypto Sims";                          // Set the name for display purposes
+       symbol = "SIMS";                               // Set the symbol for display purposes
+       decimals = 0; 
+    }
+    
+    
+ // constructor() ERC721Full("Crypto Sims", "SIMS") public {
+         
+ // }
+
+ uint maxSims = 100000; //100k
+//totalSupply = 0;
+  //only creater makes people
+  function mint(string memory _name, string memory _gender, string memory _haircolor, string memory _eyecolor, string memory _headcolor, string memory _secret) public payable {
+  require( totalSupply <= maxSims);// 100k sims can only be created
+    require(!_botNameExists[_name]);
+     // require(msg.sender == owner) ; 
+    require(msg.value == claimPrice || msg.value >= claimPrice );  
+   id = digitmon.length;
+    digitmon.push(Digitmon(_name, _gender, _haircolor, _eyecolor, _headcolor, _secret,1, msg.sender));
+     sims.push(simWearables(0,0,0,0));
+   _mint(msg.sender, id);//create them but dont mint here
+   
+    totalSupply += digitmon.length;//update total supply
+    monstersIndexToAddress[id] = msg.sender;//put address of minter inside here
+    _botNameExists[_name] = true;
+     owner.transfer(claimPrice );
+  }
+  
+  //make payable
+     function changeName(uint _id, string memory _name) public payable{
+          require(msg.value == changePrice || msg.value >= changePrice); 
+            
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        Digitmon  storage userDigitMon = digitmon[_id];
+         _botNameExists[userDigitMon.name] = false;// remove old name so others can use it 
+        
+        userDigitMon.name = _name;
+        _botNameExists[_name] = true;// re add new name
+          owner.transfer(changeItemPrice );
+  }
+
+
+    //make payable
+     function changeGender(uint _id, string memory _g) public payable{
+          require(msg.value == changeItemPrice); 
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        Digitmon  storage userDigitMon = digitmon[_id];
+        userDigitMon.gender = _g;
+          owner.transfer(changeItemPrice );
+        
+  }
+
+//make payable
+     function changeHair(uint _id, string memory _h) public payable{
+          require(msg.value == changeItemPrice); 
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        Digitmon  storage userDigitMon = digitmon[_id];
+        userDigitMon.hairColor = _h;
+          owner.transfer(changeItemPrice );
+  }
+
+//make payable
+     function changeEye(uint _id, string memory _e) public payable{
+          require(msg.value == changeItemPrice); 
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        Digitmon  storage userDigitMon = digitmon[_id];
+        userDigitMon.eyeColor = _e;
+        owner.transfer(changeItemPrice );
+  }
+  
+  //make payable
+     function changeHead(uint _id, string memory _headcolor) public payable{
+          require(msg.value == changeItemPrice); 
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        Digitmon  storage userDigitMon = digitmon[_id];
+        userDigitMon.headColor = _headcolor;
+        owner.transfer(changeItemPrice );
+  }
+
+
+ 
+  //make payable
+     function changeSecret(uint _id, string memory _secret) public payable{
+            require(msg.value == changePrice || msg.value >= changePrice); 
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        Digitmon  storage userDigitMon = digitmon[_id];
+        userDigitMon.secret = _secret;
+        owner.transfer(changePrice );
+  }
+
+
+  //make payable
+     function giveLike(uint _id) public payable{
+           require(msg.value == changePrice || msg.value >= changePrice);
+      // require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        Digitmon  storage userDigitMon = digitmon[_id];
+        userDigitMon.likes += 1;
+        owner.transfer(likesPrice);
+  }
+
+
+
+  //make payable
+     function equipA(uint _id) public payable{
+          require(msg.value >= changeItemPrice || msg.value == changeItemPrice); 
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        simWearables  storage simbag = sims[_id];
+        simbag.a += 1;
+        owner.transfer(changeItemPrice);
+  }
+
+  //make payable
+     function equipB(uint _id) public payable{
+            require(msg.value >= changeItemPrice || msg.value == changeItemPrice);  
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        simWearables  storage simbag = sims[_id];
+        simbag.b += 1;
+        owner.transfer(changeItemPrice);
+  }
+
+//make payable
+     function equipC(uint _id) public payable{
+          require(msg.value >= changeItemPrice || msg.value == changeItemPrice);  
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        simWearables  storage simbag = sims[_id];
+        simbag.c += 1;
+        owner.transfer(changeItemPrice);
+  }
+
+  
+  //make payable
+     function equipD(uint _id) public payable{
+           require(msg.value >= changeItemPrice || msg.value == changeItemPrice); 
+       require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+        simWearables  storage simbag = sims[_id];
+        simbag.d += 1;
+        owner.transfer(changeItemPrice);
+  }
+
+  
+  
+  
+  
+  
+  
+    // train your bot
+    function Train_Digimon(uint _id) public {
+       //require(msg.value != claimPrice) ;  
+       
+    require(monstersIndexToAddress[_id] == msg.sender);//check if address of sender is equal to the id
+    
+    
+    Digitmon  storage userDigitMon = digitmon[_id];
+     
+    //  userDigitMon.level += 1;
+     //  userDigitMon.health += 110;
+      //  userDigitMon.powerLevel += 110;
+        //check levels can train and grow levels until level 50
+     //   if(userDigitMon.level == 5){
+      //      userDigitMon.weapon = "Plasma Ray";
+      /////  }
+       // if(userDigitMon.level == 10){
+        //    userDigitMon.weapon = "Crystal Suit";
+      //  }
+       
+       
+    //  owner.transfer(claimPrice);
+         // DigitMon.digitmon[].level++;
+        
+          
+    }
+  
+
+
+  
+   
+}
